cb2cb5aca158b3c09fc83e59b298ec16d18901c2
Pato de Sapato <felipefernanalves@gmail.com>
2023-01-27 12:45:45 -0300

    Css pagina de criar conta

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,17 @@
 import util from "./util.js";
 
+let isLogged = window.localStorage.getItem('isLogged');
+
+if (isLogged == 'true') {
+    document.querySelector('form#login').style.display = 'none'
+    document.querySelector('#roomSearch').style.display = 'flex'
+    document.querySelector('figure h1').innerHTML = window.localStorage.getItem('username')
+} else {
+    document.querySelector('form#login').style.display = 'flex'
+    document.querySelector('#roomSearch').style.display = 'none'
+    document.querySelector('figure h1').innerHTML = 'FantasmagorE'
+}
+
 document.querySelector('#logbutton').addEventListener("click", e =>{
     let userInfos = {
         username: document.querySelector('#username').value,
